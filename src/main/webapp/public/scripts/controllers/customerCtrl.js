0061b3c13538cdd37ae70253f3c0f24f180424b8
Mario Torres <mario.torres.molina@gmail.com>
2015-02-19 16:59:07 -0500

    customer ui

--- src/main/webapp/public/scripts/controllers/customerCtrl.js
+++ src/main/webapp/public/scripts/controllers/customerCtrl.js
@@ -1,155 +1,125 @@
 'use strict';
 
+
 invoicecApp.controller('CustomerCtrl', 
-          ['$scope','Restangular','$modal',
-           function ($scope,Restangular,$modal) {
+              ['$scope','Restangular','$modal', 
+              function ($scope,Restangular,$modal) {
 
-                $scope.inInquiry=false;
-                $scope.inEdit = false;
-                $scope.tipoIdentificacion={};
-                $scope.moneda={};
-                var rDocumentTypes = Restangular.all('customer/idTypes');
-                rDocumentTypes.getList().then(function(response){
-                        $scope.listIdTypes =response.data;
-                });
+                          $scope.inInquiry=false;
+                          $scope.inEdit = false;
+                          $scope.tipoIdentificacion={};
+                          $scope.moneda={};
+                          
+                          var rDocumentTypes = Restangular.all('customer/idTypes');
+                          rDocumentTypes.getList().then(function(response){
+                            $scope.listIdTypes =response.data;
+                          });
 
-                var rEmitters = Restangular.all('emitter/list');
-                rEmitters.getList().then(function(response){
-                        $scope.listEmitter =response.data;
-                });
+                          var rEmitters = Restangular.all('emitter/list');
+                          rEmitters.getList().then(function(response){
+                            $scope.listEmitter =response.data;
+                          });
 
-                var rCurrency = Restangular.all('document/currencyTypes');
-                rCurrency.getList().then(function(response){
-                        $scope.listIdCurrency =response.data;
-                });
 
-                $scope.loadPage=function(){
-                    var customers = Restangular.all('customer/list');
-                    customers.getList().then(function(response) {
+                          var rCurrency = Restangular.all('document/currencyTypes');
+                          rCurrency.getList().then(function(response){
+                            $scope.listIdCurrency =response.data;
+                          });
+
+                          $scope.loadPage = function() {
+                                    var customers = Restangular.all('customer/list');
+                                    customers.getList().then(function(response){
                                             $scope.listCustomer=response.data;
-                    });             
-                }  
+                                    });
+                          };
 
-                $scope.save = function() {
-                  $scope.client.ruc = $scope.ruc.ruc;
-                  $scope.client.tipoIdentificacion = $scope.tipoIdentificacion.codigo;
-
-                  Restangular.all('customer').post($scope.client).then(function(response) {
-                    console.log(response.data);
-                    $scope.loadPage();
-                  }, 
-                  function(response) {
-                    console.log(response.data);
-                  });
-                };
-
-                 $scope.openModalDelete = function(customer) {
-                      $scope.items = [customer];
-                      var modalInstance = $modal.open({
-                                templateUrl: 'DelModalContent.html',
-                                controller: 'DelModalInstanceCtrl',
-                                resolve: { items: function () { return $scope.items; } }
-                      });
-
-                      modalInstance.result.then(
-                            function() {
-                                Restangular.one('customer').remove({id:$scope.items[0].rucCliente,emitterId:$scope.items[0].ruc}).then(function()
-                                                { $scope.loadPage(); });
-                            }, 
-                            function () {});
-                  };
-
-                  $scope.clear = function() {
-                      $scope.client = {};
-                      $scope.ruc=null;
-                      $scope.tipoIdentificacion={};
-                      $scope.inInquiry=false;
-                      $scope.inEdit=false;
-                    };
-
-                  $scope.inquiry = function(index) {
-                      $scope.inInquiry = true;
-                      $scope.inEdit = false;
-                      $scope.client = $scope.listCustomer[index];
-                      $scope.tipoIdentificacion=$scope.getIdType($scope.listCustomer[index].tipoIdentificacion);
-                      $scope.ruc=$scope.getEmitter($scope.listCustomer[index].ruc);
-                  };
-                    
-                  $scope.edit = function(index) {
-                      $scope.inInquiry = false;
-                      $scope.inEdit = true;
-                      $scope.client = $scope.listCustomer[index];
-                      $scope.tipoIdentificacion=$scope.getIdType($scope.listCustomer[index].tipoIdentificacion);
-                      $scope.ruc=$scope.getEmitter($scope.listCustomer[index].ruc);
-                    };
+                          $scope.save = function() {
+                                    $scope.client.identificationEmitter = $scope.emitterSelected.identification;
+                                    $scope.client.identificationTypeCode = $scope.identificationTypeSelected.codigo;                            
+                                    Restangular.all('customer').post($scope.client).then(function(response) {
+                                      console.log(response.data);
+                                      $scope.loadPage();
+                                      $scope.clear();
+                                    }, 
+                                    function(response) {
+                                      console.log(response.data);
+                                    });
+                          };
 
 
+                          $scope.openModalDelete = function(customer) {
+                                    $scope.items = [customer];
+                                    var modalInstance = $modal.open({
+                                                  templateUrl: 'DelModalContent.html',
+                                                  controller: 'DelModalInstanceCtrl',
+                                                  resolve: { items: function () { return $scope.items; } }
+                                    });
 
-                    $scope.getIdType=function(idType) {
-                      var type = {};
-                      angular.forEach($scope.listIdTypes, function(item) {
-                        if (item.codigo === idType) {
-                          type= item;
-                        }
-                      });
-                      return type;
-                    };
-
-                    $scope.getEmitter=function(id) {
-                      var emit = {};
-                      angular.forEach($scope.listEmitter, function(item) {
-                        if (item.ruc === id) {
-                          emit=item;
-                        }
-                      });
-                      return emit;
-                    }; 
-
-                  
-
-                    $scope.validInput = function () {
-                        if (!$scope.isUndefinedOrNull($scope.client)) {
-
-                          if ($scope.isUndefinedOrNull($scope.client.email)) {
-                            return false;
-                          }
-
-                          if ($scope.isUndefinedOrNull($scope.client.rucCliente)) {
-                            return false;
-                          }
-
-                          if ($scope.isUndefinedOrNull($scope.client.razonSocial)) {
-                            return false;
-                          }
-
-                          if ($scope.isUndefinedOrNull($scope.client.direccion)) {
-                            return false;
-                          }
-
-                          if ($scope.isUndefinedOrNull($scope.client.telefono)) {
-                            return false;
-                          }
-
-                          if ($scope.inInquiry) return false;
-
-                          return true;
-
-                        }
+                                    modalInstance.result.then(
+                                                  function () {
+                                                        Restangular.one('customer').remove({identification:$scope.items[0].identification, 
+                                                                                 identificationEmitter:$scope.items[0].identificationEmitter}).then(
+                                                                                                  function(){ $scope.loadPage(); });
+                                                  }, 
+                                                  function () {}
+                                    );
+                          };
+                          
+                          $scope.inquiry = function(index) {
+                                    $scope.inInquiry = true;
+                                    $scope.inEdit = false;
+                                    $scope.client = $scope.listCustomer[index];
+                                    $scope.identificationTypeSelected = $scope.getIdType($scope.listCustomer[index].identificationTypeCode);
+                                    $scope.emitterSelected=$scope.getEmitter($scope.listCustomer[index].identificationEmitter);
+                          };
 
 
-                      };
+                          $scope.edit = function(index) {
+                                      $scope.inInquiry = false;
+                                      $scope.inEdit = true;
+                                      $scope.client = $scope.listCustomer[index];
+                                      $scope.identificationTypeSelected = $scope.getIdType($scope.listCustomer[index].identificationTypeCode);
+                                      $scope.emitterSelected=$scope.getEmitter($scope.listCustomer[index].identificationEmitter);                                      
+                          };
 
-                    $scope.isUndefinedOrNull = function(val) {
-                          return angular.isUndefined(val) || val === null || val === '';
-                    };
+                          $scope.clear = function() {
+                                    $scope.client = null;
+                                    $scope.emitterSelected=null;
+                                    $scope.identificationTypeSelected=null;
+                                    $scope.inInquiry=false;
+                                    $scope.inEdit=false;
+                          };
 
+                          $scope.validInput = function () {
+                                if (!$scope.isUndefinedOrNull($scope.client)) {
+                                    if ($scope.isUndefinedOrNull($scope.client.email)) { return false; }
+                                    if ($scope.isUndefinedOrNull($scope.client.rucCliente)) { return false;}
+                                    if ($scope.isUndefinedOrNull($scope.client.razonSocial)) { return false; }
+                                    if ($scope.isUndefinedOrNull($scope.client.direccion)) { return false; }
+                                    if ($scope.isUndefinedOrNull($scope.client.telefono)) { return false; }
+                                    if ($scope.inInquiry) return false;
+                                    return true;
+                                }
+                          };
+                          $scope.getIdType=function(idType) {
+                                var type = {};
+                                angular.forEach($scope.listIdTypes, function(item) {
+                                            if (item.codigo === idType) { type= item; }
+                                });
+                                return type;
+                          };
 
-                   /**$scope.refreshList = function() {
-                       Restangular.all('customer').get('list').then(function(response) {
-                          $scope.listCustomer=response.data;
-                        });
-                    };**/
+                          $scope.getEmitter=function(id) {
+                                var emit = {};
+                                angular.forEach($scope.listEmitter, function(item) {
+                                          if (item.identification === id) { emit=item; }
+                                });
+                                return emit;
+                          }; 
 
+                          $scope.isUndefinedOrNull = function(val) {
+                                return angular.isUndefined(val) || val === null || val === '';
+                          };
 }]);
 
 
